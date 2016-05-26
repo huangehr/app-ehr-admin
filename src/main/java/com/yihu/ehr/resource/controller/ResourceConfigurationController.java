767dce6af9ce8dd25f9db0846533014d3744b295
wq <wq@qq.com>
2016-05-26 18:13:46 +0800

    资源配置表格数据加载、搜索、新增

--- src/main/java/com/yihu/ehr/resource/controller/ResourceConfigurationController.java
+++ src/main/java/com/yihu/ehr/resource/controller/ResourceConfigurationController.java
@@ -1,5 +1,6 @@
 package com.yihu.ehr.resource.controller;
 
+import com.yihu.ehr.api.ServiceApi;
 import com.yihu.ehr.util.Envelop;
 import com.yihu.ehr.util.HttpClientUtil;
 import com.yihu.ehr.util.controller.BaseUIController;
@@ -38,21 +39,22 @@ public class ResourceConfigurationController extends BaseUIController {
     @RequestMapping("searchResourceconfiguration")
     @ResponseBody
     public Object searchResourceconfiguration(String searchNm, int page, int rows){
-        Envelop envelop = new Envelop();
         Map<String, Object> params = new HashMap<>();
-
+        String metaDataUrl = ServiceApi.Resources.Metadatas;
         String resultStr = "";
 
         params.put("filters","");
         if (!StringUtils.isEmpty(searchNm))
-            params.put("filters","code?"+searchNm);//test_code
+            params.put("filters","name?" + searchNm + " g1;stdCode?" + searchNm +" g1");
 
         params.put("page", page);
         params.put("size", rows);
+        params.put("fields","");
+        params.put("sorts","");
 
         try{
 
-            resultStr = HttpClientUtil.doGet(comUrl + "search",params,username,password);
+            resultStr = HttpClientUtil.doGet(comUrl + metaDataUrl,params,username,password);
 
         }catch (Exception e){
 
@@ -63,22 +65,28 @@ public class ResourceConfigurationController extends BaseUIController {
 
     @RequestMapping("searchSelResourceconfiguration")
     @ResponseBody
-    public Object searchSelResourceconfiguration(String searchNm, int page, int rows){
-        Envelop envelop = new Envelop();
+    public Object searchSelResourceconfiguration(String searchNm,String resourcesId, int page, int rows){
         Map<String, Object> params = new HashMap<>();
+        String ResourceMetadataUrl = ServiceApi.Resources.ResourceMetadatas;
 
         String resultStr = "";
 
         params.put("filters","");
         if (!StringUtils.isEmpty(searchNm))
-            params.put("filters","code?"+searchNm);//test_code
+            params.put("filters","name?" + searchNm + " g1;stdCode?" + searchNm +" g1");
+
+        params.put("resources_id","");
+        if (!StringUtils.isEmpty(resourcesId))
+            params.put("resources_id",resourcesId);
 
         params.put("page", page);
         params.put("size", rows);
+        params.put("fields","");
+        params.put("sorts","");
 
         try{
 
-            resultStr = HttpClientUtil.doGet(comUrl + "search",params,username,password);
+            resultStr = HttpClientUtil.doGet(comUrl + ResourceMetadataUrl,params,username,password);
 
         }catch (Exception e){
 
@@ -91,26 +99,27 @@ public class ResourceConfigurationController extends BaseUIController {
     @ResponseBody
     public String saveResourceconfiguration(String addRowDatas, String delRowDatas) {
 
-        Envelop envelop = new Envelop();
         Map<String, Object> params = new HashMap<>();
         String resultStr = "";
+        String delMetaDataUrl = ServiceApi.Resources.ResourceMetadatas;
+        String addMetaDataUrl = ServiceApi.Resources.ResourceMetadatasBatch;
 
         try {
             if (!StringUtils.isEmpty(delRowDatas)) {
                 //执行删除操作
-                params.put("delRowDatas", delRowDatas);
-                resultStr = HttpClientUtil.doDelete(comUrl + "del", params, username, password);
+//                params.put("delRowDatas", delRowDatas);
+//                resultStr = HttpClientUtil.doDelete(comUrl + delMetaDataUrl, params, username, password);
             }
 
             if (!StringUtils.isEmpty(addRowDatas)) {
                 //执行新增操作
-                params.put("addRowDatas", addRowDatas);
-                resultStr = HttpClientUtil.doPost(comUrl + "add", params, username, password);
+                params.put("metadatas", addRowDatas);
+                resultStr = HttpClientUtil.doPost(comUrl + addMetaDataUrl, params, username, password);
             }
         } catch (Exception e) {
 
         }
 
-        return toJson(envelop);
+        return resultStr;
     }
 }
