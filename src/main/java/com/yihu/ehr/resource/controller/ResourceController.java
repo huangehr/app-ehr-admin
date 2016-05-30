b455dffa7265f25f808a9be60db65eade8bd9f7a
yww <yww@163.com>
2016-05-30 18:13:50 +0800

    资源注册页面功能新增

---
+++ src/main/java/com/yihu/ehr/resource/controller/ResourceController.java
@@ -0,0 +1,197 @@
+package com.yihu.ehr.resource.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.yihu.ehr.agModel.resource.RsResourcesModel;
+import com.yihu.ehr.constants.ErrorCode;
+import com.yihu.ehr.util.Envelop;
+import com.yihu.ehr.util.HttpClientUtil;
+import com.yihu.ehr.util.controller.BaseUIController;
+import com.yihu.ehr.util.log.LogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by yww on 2016/5/27.
+ */
+@Controller
+@RequestMapping("/resource/resourceManage")
+public class ResourceController extends BaseUIController {
+    @Value("${service-gateway.username}")
+    private String username;
+    @Value("${service-gateway.password}")
+    private String password;
+    @Value("${service-gateway.url}")
+    private String comUrl;
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @RequestMapping("/initial")
+    public String resourceInitial(Model model){
+        model.addAttribute("contentPage","/resource/resourcemanage/resource");
+        return "pageView";
+    }
+    @RequestMapping("/infoInitial")
+    public String resourceInterfaceInfoInitial(Model model,String id,String mode){
+        model.addAttribute("mode",mode);
+        model.addAttribute("contentPage","/resource/resourcemanage/resourceInfoDialog");
+        Envelop envelop = new Envelop();
+        String envelopStr = "";
+        try{
+            if (!StringUtils.isEmpty(id)) {
+                String url = "/resources/"+id;
+                envelopStr = HttpClientUtil.doGet(comUrl + url, username, password);
+            }
+            model.addAttribute("envelop",StringUtils.isEmpty(envelopStr)?objectMapper.writeValueAsString(envelop):envelopStr);
+        }catch (Exception ex){
+            LogService.getLogger(ResourceInterfaceController.class).error(ex.getMessage());
+        }
+        return "simpleView";
+    }
+    //配置授权浏览页面跳转
+    @RequestMapping("/switch")
+    public String switchToPage(Model model,String PageName){
+        if("config".equals(PageName)){
+            model.addAttribute("contentPage","/resource/resourcemanage/resource");
+        }
+        if("grant".equals(PageName)){
+            model.addAttribute("contentPage","/resource/resourcemanage/resource");
+        }
+        if("browse".equals(PageName)){
+            model.addAttribute("contentPage","/resource/resourcemanage/resource");
+        }
+        return "pageView";
+    }
+
+    //分页查询
+    @RequestMapping("/resources")
+    @ResponseBody
+    public Object searchResources(String searchNm,int page,int rows){
+        String url = "/resources";
+        String resultStr = "";
+        Envelop envelop = new Envelop();
+        Map<String, Object> params = new HashMap<>();
+        StringBuffer stringBuffer = new StringBuffer();
+        if (!StringUtils.isEmpty(searchNm)) {
+            stringBuffer.append("code?" + searchNm + " g1;name?" + searchNm + " g1;");
+        }
+        params.put("filters", "");
+        String filters = stringBuffer.toString();
+        if (!StringUtils.isEmpty(filters)) {
+            params.put("filters", filters);
+        }
+        params.put("page", page);
+        params.put("size", rows);
+        try {
+            resultStr = HttpClientUtil.doGet(comUrl + url, params, username, password);
+            return resultStr;
+        } catch (Exception ex) {
+            LogService.getLogger(ResourceController.class).error(ex.getMessage());
+            envelop.setSuccessFlg(false);
+            envelop.setErrorMsg(ErrorCode.SystemError.toString());
+            return envelop;
+        }
+    }
+
+    //更新
+    @RequestMapping("/update")
+    @ResponseBody
+    public Object updateResource(String dataJson,String mode){
+        Envelop envelop = new Envelop();
+        envelop.setSuccessFlg(false);
+        String url = "/resources";
+        try{
+            RsResourcesModel model = objectMapper.readValue(dataJson, RsResourcesModel.class);
+            if(StringUtils.isEmpty(model.getCode())){
+                envelop.setErrorMsg("资源编码不能为空！");
+                return envelop;
+            }
+            if(StringUtils.isEmpty(model.getName())){
+                envelop.setErrorMsg("资源名称不能为空！");
+                return envelop;
+            }
+            if("new".equals(mode)){
+                Map<String,Object> args = new HashMap<>();
+                args.put("resource",objectMapper.writeValueAsString(model));
+                String envelopStr = HttpClientUtil.doPost(comUrl+url,args,username,password);
+                return envelopStr;
+            } else if("modify".equals(mode)){
+                String urlGet = "/resources/"+model.getId();
+                String envelopGetStr = HttpClientUtil.doGet(comUrl+urlGet,username,password);
+                Envelop envelopGet = objectMapper.readValue(envelopGetStr,Envelop.class);
+                if (!envelopGet.isSuccessFlg()){
+                    envelop.setErrorMsg("原资源信息获取失败！");
+                }
+                RsResourcesModel updateModel = getEnvelopModel(envelopGet.getObj(),RsResourcesModel.class);
+                updateModel.setCode(model.getCode());
+                updateModel.setName(model.getName());
+                updateModel.setCategoryId(model.getCategoryId());
+                updateModel.setRsInterface(model.getRsInterface());
+                updateModel.setGrantType(model.getGrantType());
+                updateModel.setDescription(model.getDescription());
+                String updateModelJson = objectMapper.writeValueAsString(updateModel);
+                Map<String,Object> params = new HashMap<>();
+                params.put("resource",updateModelJson);
+                String envelopStr = HttpClientUtil.doPut(comUrl+url,params,username,password);
+                return envelopStr;
+            }
+        }catch (Exception ex){
+            LogService.getLogger(ResourceController.class).error(ex.getMessage());
+            envelop.setErrorMsg(ErrorCode.SystemError.toString());
+        }
+        return envelop;
+    }
+
+    //删除
+    @RequestMapping("/delete")
+    @ResponseBody
+    public Object deleteResource(String id) {
+        String url = "/resources/" + id;
+        String resultStr = "";
+        Envelop result = new Envelop();
+        Map<String, Object> params = new HashMap<>();
+        params.put("id", id);
+        try {
+            resultStr = HttpClientUtil.doDelete(comUrl + url, params, username, password);
+            result = objectMapper.readValue(resultStr, Envelop.class);
+            if (result.isSuccessFlg()) {
+                result.setSuccessFlg(true);
+            } else {
+                result.setSuccessFlg(false);
+                result.setErrorMsg(ErrorCode.InvalidDelete.toString());
+            }
+            return result;
+        } catch (Exception e) {
+            result.setSuccessFlg(false);
+            result.setErrorMsg(ErrorCode.SystemError.toString());
+            return result;
+        }
+
+    }
+    //资源名称唯一性验证
+    @RequestMapping("/isExistCode")
+    @ResponseBody
+    public Object isExistCode(String code){
+        Envelop envelop = new Envelop();
+        envelop.setSuccessFlg(false);
+        return envelop;
+    }
+
+    @RequestMapping("/isExistName")
+    @ResponseBody
+    public Object isExistName(String name){
+        Envelop envelop = new Envelop();
+        envelop.setSuccessFlg(false);
+        return envelop;
+    }
+
+
+
+}
---
+++ src/main/webapp/WEB-INF/ehr/jsp/resource/resourcemanage/resourceInfoDialogJs.jsp
@@ -0,0 +1,132 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"  pageEncoding="utf-8"%>
+<%@include file="/WEB-INF/ehr/commons/jsp/commonInclude.jsp" %>
+
+<script type="text/javascript">
+
+	(function ($, win) {
+		/* ************************** 变量定义 ******************************** */
+		var Util = $.Util;
+		var rsInfoForm = null;
+		// 表单校验工具类
+		var jValidation = $.jValidation;
+		var catalogDictId = 32;
+		var mode = '${mode}';
+		/* *************************** 函数定义 ******************************* */
+		function pageInit() {
+			rsInfoForm.init();
+		}
+		/* *************************** 模块初始化 ***************************** */
+		rsInfoForm = {
+			$form: $("#div_rs_info_form"),
+			$catalog: $("#inp_category"),
+			$name: $("#inp_name"),
+			$code: $("#inp_code"),
+			$interface: $("#inp_interface"),
+			$description: $("#inp_description"),
+			$btnSave: $("#btn_save"),
+			$btnCancel: $("#btn_cancel"),
+
+			init: function () {
+				this.initForm();
+				this.bindEvents();
+			},
+			initForm: function () {
+				this.initDDL();
+				this.$name.ligerTextBox({width:240});
+				this.$code.ligerTextBox({width:240});
+				this.$description.ligerTextBox({width:240, height: 120 });
+				var mode = '${mode}';
+				if(mode == 'view'){
+					rsInfoForm.$form.addClass('m-form-readonly');
+					$("#btn_save").hide();
+					$("#btn_cancel").hide();
+				}
+				this.$form.attrScan();
+				if(mode !='new'){
+					var info = ${envelop}.obj;
+					this.$form.Fields.fillValues({
+						id:info.id,
+						code:info.code,
+						name:info.name,
+						categoryId:info.categoryId,
+						rsInterface:info.rsInterface,
+						grantType:info.grantType,
+						description:info.description
+					});
+				}
+				this.$form.show();
+			},
+			initDDL: function () {
+				this.$catalog.ligerComboBox({
+					url:"${contextRoot}/resource/resourceInterface/searchRsInterfaces",
+					dataParmName: 'detailModelList',
+					urlParms: {
+						searchNm: '',
+						page:1,
+						rows:15
+					},
+					valueField: 'resourceInterface',
+					textField: 'name',
+					width:240
+				});
+
+				this.$interface.ligerComboBox({
+					url: "${contextRoot}/resource/resourceInterface/searchRsInterfaces",
+					dataParmName: 'detailModelList',
+					urlParms: {
+						searchNm: '',
+						page:1,
+						rows:15
+					},
+					valueField: 'resourceInterface',
+					textField: 'name',
+					width:240
+				});
+			},
+
+			<%--initDDL: function (dictId, target) {--%>
+				<%--target.ligerComboBox({--%>
+					<%--url: "${contextRoot}/dict/searchDictEntryList",--%>
+					<%--dataParmName: 'detailModelList',--%>
+					<%--urlParms: {dictId: dictId},--%>
+					<%--valueField: 'code',--%>
+					<%--textField: 'value'--%>
+				<%--});--%>
+			<%--},--%>
+
+			bindEvents: function () {
+				var self = this;
+				var validator =  new jValidation.Validation(this.$form, {immediate:true,onSubmit:false,
+					onElementValidateForAjax:function(elm){
+						//TODO 资源名称不重复验证
+					}
+				});
+
+				this.$btnSave.click(function () {
+					if(validator.validate()){
+						var values = self.$form.Fields.getValues();
+						var dataModel = $.DataModel.init();
+						dataModel.updateRemote("${contextRoot}/resource/resourceManage/update", {
+							data:{dataJson:JSON.stringify(values),mode:mode},
+							success: function(data) {
+								if (data.successFlg) {
+									parent.reloadMasterUpdateGrid();
+									$.Notice.success('操作成功');
+									win.closeRsInfoDialog();
+								} else {
+									$.Notice.error('操作失败！');
+								}
+							}
+						});
+					}
+				});
+
+				this.$btnCancel.click(function () {
+					win.closeRsInfoDialog();
+				});
+			}
+		};
+		/* *************************** 页面初始化 **************************** */
+		pageInit();
+	})(jQuery, window);
+</script>
