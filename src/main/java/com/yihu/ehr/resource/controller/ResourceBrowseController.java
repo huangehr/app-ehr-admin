3232f3f510e2a5bd6902a2ff8a1fe98b689997d6
wq <wq@qq.com>
2016-07-21 09:22:26 +0800

    删除一个多余的包

--- src/main/java/com/yihu/ehr/resource/controller/ResourceBrowseController.java
+++ src/main/java/com/yihu/ehr/resource/controller/ResourceBrowseController.java
@@ -15,7 +15,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
-import sun.plugin.javascript.navig.Array;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.OutputStream;
