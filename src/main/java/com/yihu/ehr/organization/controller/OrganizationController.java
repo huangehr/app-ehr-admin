98113ee1eec10dc2f969006f98e152f2aaa883ef
unknown <2522464409@qq.com>
2017-05-25 20:29:40 +0800

    构授权配置 查询机构及部门 -by zdm

--- src/main/java/com/yihu/ehr/organization/controller/OrganizationController.java
+++ src/main/java/com/yihu/ehr/organization/controller/OrganizationController.java
@@ -1,11 +1,16 @@
 package com.yihu.ehr.organization.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.yihu.ehr.adapter.service.PageParms;
+import com.yihu.ehr.agModel.dict.SystemDictEntryModel;
 import com.yihu.ehr.agModel.fileresource.FileResourceModel;
+import com.yihu.ehr.agModel.org.OrgDeptListModel;
+import com.yihu.ehr.agModel.org.OrgDeptModel;
 import com.yihu.ehr.agModel.org.OrgDetailModel;
 import com.yihu.ehr.agModel.org.OrgModel;
 import com.yihu.ehr.agModel.user.UserDetailModel;
 import com.yihu.ehr.constants.ErrorCode;
+import com.yihu.ehr.model.org.MOrgDept;
 import com.yihu.ehr.patient.controller.PatientController;
 import com.yihu.ehr.util.rest.Envelop;
 import com.yihu.ehr.controller.BaseUIController;
@@ -13,6 +18,8 @@ import com.yihu.ehr.util.HttpClientUtil;
 import com.yihu.ehr.web.RestTemplates;
 import com.yihu.ehr.util.encode.*;
 import com.yihu.ehr.util.log.LogService;
+import io.swagger.annotations.ApiOperation;
+import org.apache.commons.collections.map.HashedMap;
 import org.apache.commons.lang.ArrayUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
@@ -21,6 +28,7 @@ import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
@@ -552,6 +560,78 @@ public class OrganizationController extends BaseUIController {
             }
         }
         return url;
+    }
+
+    //获取机构部门树
+    @RequestMapping("/getAllOrgAndDepts")
+    @ResponseBody
+    public Envelop getAllOrgAndDepts() {
+        try {
+            String searchParm="";
+
+            Envelop envelop = new Envelop();
+            Map<String, Object> params = new HashMap<>();
+            //所有机构
+            String orgUrl = "/organizations/getAllOrgs";
+            String orgStr = HttpClientUtil.doGet(comUrl + orgUrl, username, password);
+            List<OrgModel> orgList = toModel(orgStr, Envelop.class).getDetailModelList();
+            //获取所有部门
+            String deptUrl = "/orgDept/getAllOrgDepts";
+            String deptStr = HttpClientUtil.doGet(comUrl + deptUrl, username, password);
+            List<OrgDeptModel> deptList = toModel(deptStr, Envelop.class).getDetailModelList();
+
+            OrgModel orgModel=null;
+            Map<String, String> orgMap = new HashedMap();
+            if (null != orgList &&orgList.size()>0) {
+                for (int i=0;i<orgList.size();i++) {
+                    orgModel = toModel(toJson(orgList.get(i)), OrgModel.class);
+                    orgMap.put(orgModel.getId().toString(), orgModel.getFullName());
+
+                }
+            }
+            OrgDeptModel orgDeptModel=null;
+            Map<String, OrgDeptListModel> orgModelMap = new HashedMap();
+            if (null != deptList && deptList.size() > 0) {
+                for (int i=0;i<deptList.size();i++) {
+                     orgDeptModel=toModel(toJson(deptList.get(i)), OrgDeptModel.class);
+                    if (null != orgModelMap.get(orgDeptModel.getOrgId())) {
+                        OrgDeptListModel od=orgModelMap.get(orgDeptModel.getOrgId());
+                        OrgDeptModel om=new OrgDeptModel();
+                        om.setCode(orgDeptModel.getCode());
+                        om.setName(orgDeptModel.getName());
+                        od.getOrgDept().add(om);
+                    }else{
+                        OrgDeptListModel od=new OrgDeptListModel();
+                        od.setOrgId(orgDeptModel.getOrgId());
+                        od.setOrgName(orgMap.get(orgDeptModel.getOrgId()));
+                        List<OrgDeptModel> list=new ArrayList<>();
+                        OrgDeptModel om=new OrgDeptModel();
+                        om.setCode(orgDeptModel.getCode());
+                        om.setName(orgDeptModel.getName());
+                        list.add(om);
+                        od.setOrgDept(list);
+                        orgModelMap.put(od.getOrgId(),od);
+                    }
+                }
+            }
+            List<OrgDeptListModel> modelList = new ArrayList<OrgDeptListModel>();
+            if(null!=orgModelMap&&orgModelMap.size()>0){
+                Iterator it = orgModelMap.keySet().iterator();
+                while (it.hasNext()) {
+                    String key = it.next().toString();
+                    modelList.add(orgModelMap.get(key));
+                }
+            }
+
+            envelop.setDetailModelList(modelList);
+            envelop.setSuccessFlg(true);
+            return envelop;
+        }
+        catch (Exception ex)
+        {
+            ex.printStackTrace();
+            return failedSystem();
+        }
     }
 
 }
