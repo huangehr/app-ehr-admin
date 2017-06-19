a527af532e82748f3de7f885064e32b820b2cca5
wangxingwang <756584395@qq.com>
2017-06-19 15:48:08 +0800

    20170619 wangxingwang 修改指标配置获取维度的方式

--- src/main/java/com/yihu/ehr/tjQuota/controller/TjDimensionSlaveController.java
+++ src/main/java/com/yihu/ehr/tjQuota/controller/TjDimensionSlaveController.java
@@ -257,14 +257,17 @@ public class TjDimensionSlaveController extends BaseUIController {
 
     @RequestMapping("/getTjDimensionSlaveInfo")
     @ResponseBody
-    public Object getTjDimensionSlaveInfo(String name, int page, int rows){
+    public Object getTjDimensionSlaveInfo(String quotaCode, String name, int page, int rows){
         String url = "/tj/getTjDimensionSlaveInfoList";
         String resultStr = "";
         Envelop envelop = new Envelop();
         Map<String, Object> params = new HashMap<>();
         StringBuffer stringBuffer = new StringBuffer();
+        if (!StringUtils.isEmpty(quotaCode)) {
+            params.put("filter", "quotaCode?" + quotaCode);
+        }
         if (!StringUtils.isEmpty(name)) {
-            stringBuffer.append("name?" + name );
+            stringBuffer.append("name?" + name + " g1;code?" + name + " g1;");
         }
         String filters = stringBuffer.toString();
         if (!StringUtils.isEmpty(filters)) {
