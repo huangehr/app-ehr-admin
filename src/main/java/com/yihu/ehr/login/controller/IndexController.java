17e53d03b5d4d04511331db84d09127eff53be61
zhangjinjun <613zjj>
2017-09-23 14:42:55 +0800

    动态获取菜单。

--- src/main/java/com/yihu/ehr/login/controller/IndexController.java
+++ src/main/java/com/yihu/ehr/login/controller/IndexController.java
@@ -1,17 +1,42 @@
 package com.yihu.ehr.login.controller;
 
+import com.yihu.ehr.agModel.user.UserDetailModel;
+import com.yihu.ehr.constants.ServiceApi;
+import com.yihu.ehr.constants.SessionAttributeKeys;
+import com.yihu.ehr.util.HttpClientUtil;
+import com.yihu.ehr.util.controller.BaseUIController;
+import com.yihu.ehr.util.rest.Envelop;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/index")
-public class IndexController {
+public class IndexController extends BaseUIController {
+
+    @Value("${service-gateway.username}")
+    private String username;
+    @Value("${service-gateway.password}")
+    private String password;
+    @Value("${service-gateway.url}")
+    private String comUrl;
 
     @RequestMapping(value = "")
-    public String init(Model model,HttpServletRequest request) {
+    public String init(Model model, HttpServletRequest request) throws Exception {
+        UserDetailModel userDetailModel = (UserDetailModel) request.getSession().getAttribute(SessionAttributeKeys.CurrentUser);
+        Map<String, Object> params = new HashMap<>();
+        params.put("appId", "zkGuSIm2Fg"); // 基础信息管理 应用
+        params.put("userId", userDetailModel.getId());
+        String envelopStr = HttpClientUtil.doGet(comUrl + ServiceApi.AppFeature.FindAppMenus, params, username, password);
+        Envelop envelop = objectMapper.readValue(envelopStr, Envelop.class);
+        String menuData = toJson(envelop.getDetailModelList());
+
+        model.addAttribute("menuData", menuData);
         model.addAttribute("contentPage", "login/empty");
         model.addAttribute("successFlg", true);
         return "frameView";
