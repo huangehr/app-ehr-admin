7fd16c82baf96dfe9c816a0a78fa22f71840acc8
xiaowei <1218706950@qq.com>
2017-11-01 16:13:38 +0800

    综合查询修改、新增数据中心首页

---
+++ src/main/java/com/yihu/ehr/resource/controller/ResourcesStatisticsController.java
@@ -0,0 +1,148 @@
+package com.yihu.ehr.resource.controller;
+
+import com.yihu.ehr.model.common.ListResult;
+import com.yihu.ehr.model.common.Result;
+import com.yihu.ehr.util.HttpClientUtil;
+import com.yihu.ehr.util.controller.BaseUIController;
+import com.yihu.ehr.util.log.LogService;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by wxw on 2017/9/12.
+ */
+@RequestMapping("/resourcesStatistics")
+@Controller
+//资源中心首页统计  控制入口
+public class ResourcesStatisticsController extends BaseUIController {
+
+    @Value("${service-gateway.username}")
+    private String username;
+    @Value("${service-gateway.password}")
+    private String password;
+    @Value("${service-gateway.url}")
+    private String comUrl;
+
+    @RequestMapping(value = "/stasticReport/getArchiveReportInfo", method = RequestMethod.GET)
+    @ResponseBody
+    @ApiOperation(value = "获取健康档案图表", notes = "获取健康档案图表")
+    public Result getArchiveReportInfo(String requestType) {
+        String url = "/stasticReport/getArchiveReportInfo";
+        String resultStr = "";
+        try {
+            Map<String, Object> params = new HashMap<>();
+            params.put("requestType", requestType);
+            resultStr = HttpClientUtil.doGet(comUrl + url, params, username, password);
+            return toModel(resultStr, ListResult.class);
+        } catch (Exception e) {
+            LogService.getLogger(ResourceInterfaceController.class).error(e.getMessage());
+        }
+        return null;
+    }
+
+    @RequestMapping(value = "/stasticReport/getStatisticsElectronicMedicalCount", method = RequestMethod.GET)
+    @ResponseBody
+    @ApiOperation(value = "电子病历-最近七天采集总数统计，门诊住院数", notes = "电子病历-最近七天采集总数统计，门诊住院数")
+    public Result getStatisticsElectronicMedicalCount( ) {
+        String url = "/stasticReport/getStatisticsElectronicMedicalCount";
+        String resultStr = "";
+        try {
+            Map<String, Object> params = new HashMap<>();
+            resultStr = HttpClientUtil.doGet(comUrl + url, params, username, password);
+            return toModel(resultStr, ListResult.class);
+        } catch (Exception e) {
+            LogService.getLogger(ResourceInterfaceController.class).error(e.getMessage());
+        }
+        return null;
+    }
+
+    @RequestMapping(value = "/stasticReport/getStatisticsMedicalEventTypeCount", method = RequestMethod.GET)
+    @ResponseBody
+    @ApiOperation(value = "电子病历 - 今天 门诊住院数统计", notes = "电子病历 - 今天 门诊住院数统计")
+    public Result getStatisticsMedicalEventTypeCount( ) {
+        String url = "/stasticReport/getStatisticsMedicalEventTypeCount";
+        String resultStr = "";
+        try {
+            Map<String, Object> params = new HashMap<>();
+            resultStr = HttpClientUtil.doGet(comUrl + url, params, username, password);
+            return toModel(resultStr, ListResult.class);
+        } catch (Exception e) {
+            LogService.getLogger(ResourceInterfaceController.class).error(e.getMessage());
+        }
+        return null;
+    }
+
+    @RequestMapping(value = "/stasticReport/getStatisticsDemographicsAgeCount", method = RequestMethod.GET)
+    @ResponseBody
+    @ApiOperation(value = "全员人口个案库 - 年龄段人数统计", notes = "全员人口个案库 - 年龄段人数统计")
+    public Result getStatisticsDemographicsAgeCount( ) {
+        String url = "/stasticReport/getStatisticsDemographicsAgeCount";
+        String resultStr = "";
+        try {
+            Map<String, Object> params = new HashMap<>();
+            resultStr = HttpClientUtil.doGet(comUrl + url, params, username, password);
+            return toModel(resultStr, ListResult.class);
+        } catch (Exception e) {
+            LogService.getLogger(ResourceInterfaceController.class).error(e.getMessage());
+        }
+        return null;
+    }
+
+    @RequestMapping(value = "/stasticReport/getStatisticsUserCards", method = RequestMethod.GET)
+    @ResponseBody
+    @ApiOperation(value = "获取健康卡绑定量", notes = "获取健康卡绑定量")
+    public Result getStatisticsUserCards( ) {
+        String url = "/tj/getStatisticsUserCards";
+        String resultStr = "";
+        try {
+            Map<String, Object> params = new HashMap<>();
+            resultStr = HttpClientUtil.doGet(comUrl + url, params, username, password);
+            return toModel(resultStr, ListResult.class);
+        } catch (Exception e) {
+            LogService.getLogger(ResourceInterfaceController.class).error(e.getMessage());
+        }
+        return null;
+    }
+
+    @RequestMapping(value = "/stasticReport/getStatisticsDoctorByRoleType", method = RequestMethod.GET)
+    @ResponseBody
+    @ApiOperation(value = "按机构医生、护士、床位的统计", notes = "按机构医生、护士、床位的统计")
+    public Result getStatisticsDoctorByRoleType( ) {
+        String url = "/tj/getStatisticsDoctorByRoleType";
+        String resultStr = "";
+        try {
+            Map<String, Object> params = new HashMap<>();
+            resultStr = HttpClientUtil.doGet(comUrl + url, params, username, password);
+            return toModel(resultStr, ListResult.class);
+        } catch (Exception e) {
+            LogService.getLogger(ResourceInterfaceController.class).error(e.getMessage());
+        }
+        return null;
+    }
+
+    @RequestMapping(value = "/stasticReport/getStatisticsCityDoctorByRoleType", method = RequestMethod.GET)
+    @ResponseBody
+    @ApiOperation(value = "全市医生、护士、床位的统计", notes = "全市医生、护士、床位的统计")
+    public Result getStatisticsCityDoctorByRoleType( ) {
+        String url = "/tj/getStatisticsCityDoctorByRoleType";
+        String resultStr = "";
+        try {
+            Map<String, Object> params = new HashMap<>();
+            resultStr = HttpClientUtil.doGet(comUrl + url, params, username, password);
+            return toModel(resultStr, ListResult.class);
+        } catch (Exception e) {
+            LogService.getLogger(ResourceInterfaceController.class).error(e.getMessage());
+        }
+        return null;
+    }
+
+
+}
