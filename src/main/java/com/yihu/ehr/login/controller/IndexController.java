442b8692e38beb0b15e102285b88491fada41037
jkzlzhoujie <08zhoujie87@163.com>
2017-11-23 11:45:25 +0800

    session 同步到redis

--- src/main/java/com/yihu/ehr/login/controller/IndexController.java
+++ src/main/java/com/yihu/ehr/login/controller/IndexController.java
@@ -1,17 +1,23 @@
 package com.yihu.ehr.login.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.yihu.ehr.agModel.user.UserDetailModel;
 import com.yihu.ehr.constants.ServiceApi;
 import com.yihu.ehr.constants.SessionAttributeKeys;
 import com.yihu.ehr.util.HttpClientUtil;
 import com.yihu.ehr.util.controller.BaseUIController;
 import com.yihu.ehr.util.rest.Envelop;
+import org.apache.commons.lang.StringUtils;
+import org.omg.CORBA.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
+import java.lang.Object;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -27,14 +33,21 @@ public class IndexController extends BaseUIController {
     private String comUrl;
     @Value("${app.baseClientId}")
     private String baseClientId;
+    @Autowired
+    public ObjectMapper objectMapper;
 
     @RequestMapping(value = "")
     public String init(Model model, HttpServletRequest request) throws Exception {
-        Object clientId = request.getSession().getAttribute("clientId");
+        String clientId = (String)request.getSession().getAttribute("clientId");
         UserDetailModel userDetailModel = (UserDetailModel) request.getSession().getAttribute(SessionAttributeKeys.CurrentUser);
-
+        if( ! StringUtils.isNotEmpty(clientId)){
+             clientId =  getRedisValue(request,"clientId");
+        }
+        if(userDetailModel == null){
+            userDetailModel = objectMapper.readValue(getRedisValue(request, SessionAttributeKeys.CurrentUser), UserDetailModel.class);
+        }
         Map<String, Object> params = new HashMap<>();
-        if (clientId != null) {
+        if (StringUtils.isNotEmpty(clientId)) {
             // 从总支撑门户过来，会附带 appId 参数
             params.put("appId", clientId.toString());
         } else {
@@ -45,7 +58,18 @@ public class IndexController extends BaseUIController {
         String envelopStr = HttpClientUtil.doGet(comUrl + ServiceApi.AppFeature.FindAppMenus, params, username, password);
         Envelop envelop = objectMapper.readValue(envelopStr, Envelop.class);
         String menuData = toJson(envelop.getDetailModelList());
-        model.addAttribute("host", request.getSession().getAttribute("host"));
+
+        String host = (String)request.getSession().getAttribute("host");
+        String lastLoginTime = (String)request.getSession().getAttribute("last_login_time");
+        if( ! StringUtils.isNotEmpty(host)){
+            host = getRedisValue(request ,"host");
+        }
+        if( ! StringUtils.isNotEmpty(lastLoginTime)){
+            lastLoginTime = getRedisValue(request ,"last_login_time");
+        }
+
+        model.addAttribute("last_login_time",lastLoginTime);
+        model.addAttribute("host",host);
         model.addAttribute("menuData", menuData);
         model.addAttribute("contentPage", "login/empty");
         model.addAttribute("successFlg", true);
